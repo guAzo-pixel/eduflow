a48b7932ff604c1a57d26bf1eb6276d096970c28
guAzo-pixel <174360675+guAzo-pixel@users.noreply.github.com>
2026-04-01 18:14:12 +0000

    Primer desde el server

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php include 'includes/header.php'; ?>
+<?php include 'includes/db.php'; ?>
+
+<main>
+    <h1>Bienvenido a Eduflow</h1>
+    <h2>Usuarios registrados</h2>
+
+    <?php
+    if ($roluser == 'admin'){
+    /*Hacemos la consulta */
+    $sql = "SELECT * FROM Users";
+
+    /*Enviamos la consulta al embajador (PDO) query es la petición*/
+    $smnt = $pdo->query($sql);
+
+    /* Procesamos los datos recibidos*/
+    $users = $smnt->fetchAll(PDO::FETCH_ASSOC);
+
+    /* Recorremos los datos obtenidos de la petición y los imprimimos*/
+    foreach($users as $user){
+        $name = $user['name'];
+        $lastname = $user['lastName'];
+        $rol = $user['rol'];
+        echo "<li>$name $lastname - Rol: $rol</li>";
+    }
+    }
+    else{
+        echo "<p>No tienes acceso</p>";
+    }
+    ?>
+</main>
+
+<?php include 'includes/footer.php'; ?>
